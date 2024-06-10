e039b577b4be6d64015e1894a275e9bc12997c7a
noppadolr <riyasarn.n@gmail.com>
2024-06-10 17:55:32 +0700

    fix sidebar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\User\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('admin/dashboard',Dashboard::class)->name('admin.dashboard');
